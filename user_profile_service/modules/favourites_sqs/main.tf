3cf2ec130c61db3339ffb530e492309d42dfeb4d
TooMuchRakja <szymekp1994@gmail.com>
2025-06-29 19:02:54 +0200

    Testing full  module 4 terraform validation 5

--- user_profile_service/modules/favourites_sqs/main.tf
+++ user_profile_service/modules/favourites_sqs/main.tf
@@ -2,7 +2,7 @@ resource "aws_sqs_queue" "favourites_sqs" {
     name = "favourites_sqs_queue"
     message_retention_seconds = 86400 # 1 day
 
-    tags {
+    tags = {
         Environment = "${var.environment}"
         stack_name =  "${var.stack_name}"
     }
