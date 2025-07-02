531fb4355b625b283fd93601d51e5ae0e919ee7a
TooMuchRakja <szymekp1994@gmail.com>
2025-07-02 23:32:30 +0200

    added version for sqs mapping - integration tests 10

--- user_profile_service/tests/integration/favourites/test_api_gateway_favourites.py
+++ user_profile_service/tests/integration/favourites/test_api_gateway_favourites.py
@@ -27,7 +27,7 @@ def test_add_user_favorite(global_config):
     assert response.status_code == 200  
 
     # ensure the async request in the queue has time to be processed
-    time.sleep(3)
+    time.sleep(10)
 
     response = requests.get(
         global_config["address_api_endpoint_url"] + '/favourites',
@@ -42,17 +42,6 @@ def test_add_user_favorite(global_config):
     assert response_data['favorites'][0]['restaurant_id'] == user1_new_favorite['restaurantId']
 
 def test_delete_user_favorite(global_config):
-    response = requests.post(
-        global_config["address_api_endpoint_url"] + '/favourites',
-        data=json.dumps(user1_new_favorite),
-        headers={'Authorization': global_config["user1UserIdToken"], 
-            'Content-Type': 'application/json'}
-    ) 
-    assert response.status_code == 200  
-
-    # ensure the async request in the queue has time to be processed
-    time.sleep(3)
-
     response = requests.delete(
         global_config["address_api_endpoint_url"] + '/favourites/' + user1_new_favorite['restaurantId'],
         headers={'Authorization': global_config["user1UserIdToken"], 
@@ -61,7 +50,7 @@ def test_delete_user_favorite(global_config):
     assert response.status_code == 200
 
     # ensure the async request in the queue has time to be processed
-    time.sleep(5)    
+    time.sleep(10)    
 
     response = requests.get(
         global_config["address_api_endpoint_url"] + '/favourites',
